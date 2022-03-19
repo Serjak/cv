405374c242c65f8efbbab6c01e942b54eaf6ce8a
Sergio Jakimenko <52475268+Serjak@users.noreply.github.com>
2022-03-19 17:14:51 -0300

    sidebar responsive

--- main.js
+++ main.js
@@ -1,104 +1,127 @@
 // Mobile Menu Toggle Button JavaScript
 function myFunction() {
-    let x = document.getElementById("mobile-menu");
-    if (x.style.display === "none") {
-      x.style.display = "block";
-    } else {
-      x.style.display = "none";
-    }
-  }
+	let x = document.getElementById("mobile-menu");
+	if (x.style.display === "none") {
+		x.style.display = "block";
+	} else {
+		x.style.display = "none";
+	}
+}
 
-  function between(x, min, max) {
-    return x >= min && x <= max;
-  }
+function between(x, min, max) {
+	return x >= min && x <= max;
+}
 
-  function softRaiting(appName, stars, altText) {
-    if (altText == null) {
-      altText = appName;
-    }
-    let divElement = document.createElement("div");
-    divElement.className = 'app-name';
-    divElement.id = appName.toLowerCase() + "-id";
+function softRaiting(appName, stars, altText) {
+	if (altText == null) {
+		altText = appName;
+	}
+	let divElement = document.createElement("div");
+	divElement.className = "app-name";
+	divElement.id = appName.toLowerCase() + "-id";
 
-    let linkElement = document.createElement("a");
-    linkElement.href = 'http://www.google.com/search?ie=utf-8&btnI=&q=' + appName;
-    linkElement.target = 'blank';
+	let linkElement = document.createElement("a");
+	linkElement.href = "http://www.google.com/search?ie=utf-8&btnI=&q=" + appName;
+	linkElement.target = "blank";
 
-    let imgElement = document.createElement('img');
-    imgElement.className = "app-logo";
-    imgElement.src = "/images/" + appName.toLowerCase() + "-logo.png";
-    imgElement.title = altText;
-    linkElement.appendChild(imgElement);
-    divElement.appendChild(linkElement);
-    divElement.appendChild(document.createElement('br'));
+	let imgElement = document.createElement("img");
+	imgElement.className = "app-logo";
+	imgElement.src = "/images/" + appName.toLowerCase() + "-logo.png";
+	imgElement.title = altText;
+	linkElement.appendChild(imgElement);
+	divElement.appendChild(linkElement);
+	divElement.appendChild(document.createElement("br"));
 
-    let starsRaiting = '';
-    if (between(stars,1,5)) {
-      for (let i = 1; i <= stars; i++) {
-        let iElement = document.createElement('i');
-        iElement.className = "fa-solid fa-star checked";
-        divElement.appendChild(iElement);
-      }
-      let rest = 5 - stars;
-      for (let i = 1; i <= rest; i++) {
-        let iElement = document.createElement('i');
-        iElement.className = "fa-solid fa-star";
-        divElement.appendChild(iElement);
-      }
-    } else {
-      for (let i = 1; i <= 5; i++) {
-        let iElement = document.createElement('i');
-        iElement.className = "fa-solid fa-star";
-        divElement.appendChild(iElement);
-      }
-    }
-    document.getElementById("skillsContainer").appendChild(divElement);
-  }
+	let starsRaiting = "";
+	if (between(stars, 1, 5)) {
+		for (let i = 1; i <= stars; i++) {
+			let iElement = document.createElement("i");
+			iElement.className = "fa-solid fa-star checked";
+			divElement.appendChild(iElement);
+		}
+		let rest = 5 - stars;
+		for (let i = 1; i <= rest; i++) {
+			let iElement = document.createElement("i");
+			iElement.className = "fa-solid fa-star";
+			divElement.appendChild(iElement);
+		}
+	} else {
+		for (let i = 1; i <= 5; i++) {
+			let iElement = document.createElement("i");
+			iElement.className = "fa-solid fa-star";
+			divElement.appendChild(iElement);
+		}
+	}
+	document.getElementById("skillsContainer").appendChild(divElement);
+}
 
-// function bounceTitles(id,sectionId) {
-//   let titleIdElement = document.getElementById(id);
-//   let sectionIdElement = document.getElementById(sectionId);
+window.addEventListener("scroll", function () {
+	let elements = document.getElementsByClassName("set__hidden__section");
+	let elementsTitle = document.getElementsByClassName("set__hidden__title");
+	let screenSize = window.innerHeight;
 
-//   sectionIdElement.classList.add('visible');
-//   titleIdElement.classList.add('visible');
-//   titleIdElement.classList.add("animate__animated");
-//   titleIdElement.classList.add("animate__bounceInLeft");
-//   sectionIdElement.classList.add("animate__animated");
+	for (var i = 0; i < elementsTitle.length; i++) {
+		let element = elementsTitle[i];
 
-//   let randomAnimation = ["animate__flipInX","animate__lightSpeedInLeft","animate__zoomIn","animate__zoomInUp"];
+		if (element.getBoundingClientRect().top < screenSize) {
+			element.classList.add("visible");
+		} else {
+			element.classList.remove("visible");
+		}
+	}
 
-//   sectionIdElement.classList.add(randomAnimation[Math.floor(Math.random() * randomAnimation.length)]);
-// }
+	for (var i = 0; i < elements.length; i++) {
+		let element = elements[i];
 
-
-window.addEventListener('scroll', function()  {
-  let elements = document.getElementsByClassName('set__hidden__section');
-  let elementsTitle = document.getElementsByClassName('set__hidden__title');
-  let screenSize = window.innerHeight;
-
-  for(var i = 0; i < elementsTitle.length; i++) {
-    let element = elementsTitle[i];
-
-    if(element.getBoundingClientRect().top < screenSize) {
-      element.classList.add('visible');
-    } else {
-      element.classList.remove('visible');
-    }
-  }
-  
-  for(var i = 0; i < elements.length; i++) {
-    let element = elements[i];
-
-    if(element.getBoundingClientRect().top < screenSize) {
-      element.classList.add('visible');
-    } else {
-      element.classList.remove('visible');
-    }
-  }
+		if (element.getBoundingClientRect().top < screenSize) {
+			element.classList.add("visible");
+		} else {
+			element.classList.remove("visible");
+		}
+	}
 });
 
+window.addEventListener("touchmove", function () {
+	if (sidebar.style.display === "unset") {
+		document
+			.getElementById("button-sidebar")
+			.classList.remove("fa-caret-right");
+		document.getElementById("button-sidebar").classList.add("fa-caret-left");
+		sidebar.style.display = "none";
+	}
+});
 
 function onClick(element) {
-  document.getElementById("modal__img").src = element.src;
-  document.getElementById("gallery__modal").style.display = "block";
+	document.getElementById("modal__img").src = element.src;
+	document.getElementById("gallery__modal").style.display = "block";
 }
+
+function toggleSideBar() {
+	sidebar = document.getElementById("sidebar");
+	sidebarButton = document.getElementById("button-sidebar");
+	if (sidebar.style.display === "unset") {
+		sidebarButton.classList.remove("fa-caret-right");
+		sidebarButton.classList.add("fa-caret-left");
+		sidebar.style.display = "none";
+	} else {
+		sidebarButton.classList.remove("fa-caret-left");
+		sidebarButton.classList.add("fa-caret-right");
+		sidebar.style.display = "unset";
+	}
+}
+
+const toggleButton = document.getElementById("button-menu");
+const navWrapper = document.getElementById("nav");
+
+toggleButton.addEventListener("touchstart", () => {
+	document.getElementById("button-menu").style.display = "none";
+	toggleButton.classList.toggle("close");
+	navWrapper.classList.toggle("show");
+});
+
+navWrapper.addEventListener("click", (e) => {
+	if (e.target.id === "nav") {
+		navWrapper.classList.remove("show");
+		toggleButton.classList.remove("close");
+	}
+});
